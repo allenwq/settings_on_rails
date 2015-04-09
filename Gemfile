0e0913ac50bbdd728e290a5a153dc89989c4d92e
WANG QIANG <rovingbreeze@gmail.com>
2015-04-09 21:45:11 +0800

    Add coverall

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in settings_on_rails.gemspec
 gemspec
+
+gem 'coveralls', require: false
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_record'
 require 'settings_on_rails'
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
   config.order = :random
